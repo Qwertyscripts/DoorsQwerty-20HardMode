156281cd9746a21f183a63d1571670d846cfcc59
QwertyS <sn001400@gmail.com>
2026-05-08 20:42:28 +0700

    HardMode%20Protected).lua to CrazyModeForOld%20Protected).lua

--- CrazyModeForOld%20Protected).lua
+++ CrazyModeForOld%20Protected).lua
@@ -6,37 +6,17 @@ task.spawn(function()
     local channel = TextChatService:WaitForChild("TextChannels", 20):FindFirstChild("RBXGeneral")
     
     if channel then
-        channel:DisplaySystemMessage("<font color='#00FFFF'><b>Hard Mode</b></font>")
+        channel:DisplaySystemMessage("<font color='#00FFFF'><b>Crazy Mode</b></font>")
     end
 end)
-
--- Depth
-coroutine.wrap(function()
-    while true do 
-        wait(math.random(20, 40))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-       wait(2)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Qwertyscripts/DoorsQwerty-20HardMode/refs/heads/main/depth.lua"))()
-end
-end)()
 
 -- 200
 coroutine.wrap(function()
     while true do 
-        wait(math.random(60, 100))
+        wait(math.random(40, 100))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
        wait(2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Qwertyscripts/DoorsQwerty-20HardMode/refs/heads/main/200.lua"))()
-end
-end)()
-
--- Silence
-coroutine.wrap(function()
-    while true do 
-        wait(math.random(20, 40))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-       wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Qwertyscripts/DoorsQwerty-20HardMode/refs/heads/main/Silence.lua"))()
 end
 end)()
 
