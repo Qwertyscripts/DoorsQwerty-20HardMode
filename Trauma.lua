400132b9bbca5edd6b95dcf3aaa9fc7d86c997e8
QwertyS <sn001400@gmail.com>
2026-05-09 21:45:44 +0700

    Trauma.lua

--- Trauma.lua
+++ Trauma.lua
@@ -1,11 +1,14 @@
 local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
 
+local killChance = 0.3 
+local willKill = math.random() <= killChance
+
 local entity = spawner.Create({
 	Entity = {
 		Name = "Trauma",
 		Asset = "rbxassetid://6685956411",
 		HeightOffset = 0,
-	    Sound = { 5375147888, { Volume = 2, Pitch = 0.95, Looped = true } }
+	    Sound = { "rbxassetid://5375147888", { Volume = 2, Pitch = 0.95, Looped = true } }
 	},
 	Lights = {
 		Flicker = {
@@ -23,38 +26,37 @@ local entity = spawner.Create({
 	CameraShake = {
 		Enabled = true,
 		Range = 75,
-		Values = {40, 100, 0.3, 1}
+		Values = {30, 100, 0.3, 1}
 	},
 	Movement = {
 		Speed = 200,
-		Delay = 20,
+		Delay = 8,
 		SpawnLocation = "Next",
-		Reversed = true
+		Reversed = true,
+        CanFly = true
 	},
 	Damage = {
-		Enabled = true,
-		Range = 20,
+		Enabled = willKill,
+		Range = 25,
 		Amount = 1000
 	},
 	Death = {
 		Type = "Guiding",
-		Hints = {"You died to Trauma", "It appears when the lights are flickering.", "Hide if it spawn", "Alright Now!"},
+		Hints = {"You died to Trauma", "It was a lethal strike..."},
 		Cause = "Trauma"
 	}
 })
-
 
 entity:SetCallback("OnSpawned", function()
     local model = entity.EntityModel
     if model then
         local primaryPart = model.PrimaryPart or model:FindFirstChildWhichIsA("BasePart")
-        
         if primaryPart then
             local light = Instance.new("PointLight")
             light.Parent = primaryPart
             light.Color = Color3.fromRGB(255, 0, 0)
-            light.Brightness = 30
-            light.Range = 40
+            light.Brightness = 25
+            light.Range = 80
             light.Shadows = true
         end
     end
