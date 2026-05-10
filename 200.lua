faf8d8dedb9d0ff525a3a8554292a00582bc637d
QwertyS <sn001400@gmail.com>
2026-05-10 09:36:46 +0700

    200.lua

--- 200.lua
+++ 200.lua
@@ -5,37 +5,22 @@ local entity = spawner.Create({
 		Name = "200",
 		Asset = "rbxassetid://12650043114",
 		HeightOffset = 0.6,
-		Sound = { "rbxassetid://5257382967", { Volume = 1, Pitch = 0.96, Looped = true } }
+		Sound = { "rbxassetid://5257382967", { Volume = 1, Pitch = 1, Looped = true } }
 	},
 	Lights = {
-		Flicker = {
-			Enabled = true,
-			Duration = 2
-		},
+		Flicker = { Enabled = true, Duration = 2 },
 		Shatter = true,
 		Repair = false
 	},
-	Earthquake = {
-		Enabled = true,
-		Distance = 150,
-		Magnitude = 5
-	},
-	CameraShake = {
-		Enabled = true,
-		Range = 75,
-		Values = {20, 50, 0.3, 1}
-	},
+	Earthquake = { Enabled = true, Distance = 150, Magnitude = 5 },
+	CameraShake = { Enabled = true, Range = 75, Values = {20, 50, 0.3, 1} },
     Movement = {
 		Speed = 0,
 		Delay = 0,
 		SpawnLocation = "Next",
 		Reversed = false
 	},
-	Damage = {
-		Enabled = true,
-		Range = 15,
-		Amount = 1000
-	},
+	Damage = { Enabled = true, Range = 15, Amount = 1000 },
 	Death = {
 		Type = "Guiding",
 		Hints = {"Wait for it to vanish...", "Don't rush through the door when it's there!"},
@@ -48,8 +33,7 @@ entity:SetCallback("OnSpawned", function()
     if model then
         local primaryPart = model.PrimaryPart or model:FindFirstChildWhichIsA("BasePart")
         if primaryPart then
-            local light = Instance.new("PointLight")
-            light.Parent = primaryPart
+            local light = Instance.new("PointLight", primaryPart)
             light.Color = Color3.fromRGB(255, 225, 225)
             light.Brightness = 10
             light.Range = 14
@@ -59,7 +43,7 @@ entity:SetCallback("OnSpawned", function()
 
     task.delay(25, function()
         if entity then
-            entity:Destroy()
+            pcall(function() entity:Destroy() end)
         end
     end)
 end)
